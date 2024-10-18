40b0e4f18ba793c543566d854212574a83d27938
David Gardner <dagardner@nvidia.com>
2024-10-18 14:05:03 -0700

    Slim down repro

--- src/repro_1956/type_aliases.py
+++ src/repro_1956/type_aliases.py
@@ -1,39 +1,7 @@
-# Copyright (c) 2022-2024, NVIDIA CORPORATION.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 """Type aliases used throughout the codebase."""
 
 import typing
 
-if typing.TYPE_CHECKING:
-    import cupy
-    import numpy
-    import pandas
-
-    import cudf
-
-DataFrameModule = typing.Literal["cudf", "pandas"]
-"""Valid DataFrame modules."""
-
-DataFrameType = typing.Union["pandas.DataFrame", "cudf.DataFrame"]
-"""Alias for pandas and cuDF DataFrame types."""
-
-SeriesType = typing.Union["pandas.Series", "cudf.Series"]
-"""Alias for pandas and cuDF Series types."""
-
-NDArrayType = typing.Union["numpy.ndarray", "cupy.ndarray"]
-"""Alias for NumPy and CuPy ndarray types."""
-
-# Using `typing.Dict[str, NDArrayType]` works-around the issue
-TensorMapType = dict[str, NDArrayType]
-"""Alias for a dictionary of tensor names to tensors represented as either a NumPy or CuPy ndarray."""
+# Using `typing.Dict[str, float]` works-around the issue
+MapType = dict[str, float]
+"""Alias for a dictionary of strings to floats"""
